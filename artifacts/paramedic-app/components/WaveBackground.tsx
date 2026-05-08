25523cb5a3afbd06126dec28c48dc3b865e6a20a
Viktor230623-M <viktor230623-m@users.noreply.github.com>
2026-05-08 21:07:56 +0200

    Fix WaveBackground with status bar transparency

--- artifacts/paramedic-app/components/WaveBackground.tsx
+++ artifacts/paramedic-app/components/WaveBackground.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, StatusBar } from "react-native";
 import Svg, { Path } from "react-native-svg";
 
 interface WaveBackgroundProps {
@@ -8,33 +8,32 @@ interface WaveBackgroundProps {
 
 export function WaveBackground({ color = "#F0FDF4" }: WaveBackgroundProps) {
   return (
-    <View style={StyleSheet.absoluteFill} pointerEvents="none">
-      <View style={StyleSheet.absoluteFill} />
-      <Svg
-        height="100%"
-        width="100%"
-        style={[StyleSheet.absoluteFill, { position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }]}
-        viewBox="0 0 400 800"
-        preserveAspectRatio="xMidYMid slice"
-      >
-        <Path
-          d="M-50 150 Q 100 80 200 160 Q 300 240 450 120 L 450 0 L -50 0 Z"
-          fill={color}
-          opacity={0.5}
-        />
-        <Path
-          d="M-50 300 Q 80 220 200 300 Q 320 380 450 280 L 450 150 Q 300 250 200 170 Q 100 90 -50 160 Z"
-          fill={color}
-          opacity={0.35}
-        />
-        <Path
-          d="M-50 700 Q 120 620 250 700 Q 360 770 450 680 L 450 800 L -50 800 Z"
-          fill={color}
-          opacity={0.4}
-        />
-      </Svg>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
+      <View style={[StyleSheet.absoluteFill, { backgroundColor: color }]} pointerEvents="none">
+        <Svg
+          height="100%"
+          width="100%"
+          viewBox="0 0 400 800"
+          preserveAspectRatio="xMidYMid slice"
+        >
+          <Path
+            d="M-50 150 Q 100 80 200 160 Q 300 240 450 120 L 450 0 L -50 0 Z"
+            fill={color}
+            opacity={0.5}
+          />
+          <Path
+            d="M-50 300 Q 80 220 200 300 Q 320 380 450 280 L 450 150 Q 300 250 200 170 Q 100 90 -50 160 Z"
+            fill={color}
+            opacity={0.35}
+          />
+          <Path
+            d="M-50 700 Q 120 620 250 700 Q 360 770 450 680 L 450 800 L -50 800 Z"
+            fill={color}
+            opacity={0.4}
+          />
+        </Svg>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({});
